44e8f8937a9172f8d1330d794760f5b80da3129e
BairSultimov <sultumovb@gmail.com>
2021-10-09 23:04:05 +0800

    no way

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         try {
+            //Ошибка= деление на ноль
             int n=6/0;
             First sort = new First("aissur.gnalorP");
             sort.reverseMassive();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        catch (Exception e) {int n=6;e.printStackTrace();}
         try {
             Bracket brac = new Bracket();
             brac.setStr("()()");
             if (brac.checkBrackets()) {
-                System.out.println("\n'(' == ')'");
+                System.out.println("\nОткрытые скобки равны закрытым");
             } else
-                System.out.println("\n'(' != ')'");
-        } catch (Exception e) {
-            e.printStackTrace();
+                System.out.println("\nОткрытые скобки не равны закрытым");
         }
+        catch (Exception e) {e.printStackTrace();}
         try {
             Third third1 = new Third("Каждый символ этой строки мечтает чтобы его пересчитали.");
             third1.outSymbol();
